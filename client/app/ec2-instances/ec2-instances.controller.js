75fd187ddbf1ae08f8bff821d380cd65cb9d6e57
wOvalle <willyovalle16@gmail.com>
2014-10-04 22:12:07 -0400

    Cleaning aws-sdk-js and dependencies. Moving to node js aws-sdk instead

--- client/app/ec2-instances/ec2-instances.controller.js
+++ client/app/ec2-instances/ec2-instances.controller.js
@@ -1,26 +1,6 @@
 'use strict';
 
 angular.module('awsTestApp')
-  .controller('Ec2InstancesCtrl', function ($scope, amazonCreds) {
-
-  AWS.config.update({accessKeyId: amazonCreds.key, secretAccessKey: amazonCreds.secret});
-
-        AWS.config.region= 'us-east-1';
-        var bucket = new AWS.S3({params: {Bucket: 'ec2-manager'}});
+  .controller('Ec2InstancesCtrl', function ($scope) {
         $scope.objects = [];
-        this.variable = "variable123";
-        bucket.listObjects(function (err, data) {
-            if (err) {
-                console.log('Error');
-            } else {
-                console.log('Loaded ' + data.Contents.length + ' items from S3');
-                console.log(data);
-                for (var i = 0; i < data.Contents.length; i++) {
-                    $scope.objects.push(data.Contents[i].Key);
-                    console.log(data.Contents[i].Key);
-                    $scope.$apply();
-                }
-            }
-        });
-
   });
