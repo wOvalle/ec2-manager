7cdc0354c40b8f4e083301c3bb37c98dc0682515
wOvalle <willyovalle16@gmail.com>
2014-10-19 20:50:39 -0400

    Templates to standalone files

--- client/app/ec2-instances/ec2-instances.controller.js
+++ client/app/ec2-instances/ec2-instances.controller.js
@@ -4,9 +4,8 @@ angular.module('awsTestApp')
     .controller('Ec2InstancesCtrl', function ($scope, awsFactory, $modal) {
         $scope.instances = [];
 
-
-        var modalInstance = $modal.open({
-            templateUrl: 'modal.html',
+        var loadingModal = $modal.open({
+            templateUrl: 'app/ec2-instances/loading.html',
             size: 'sm',
             backdrop: 'static',
             keyboard: false,
@@ -18,29 +17,24 @@ angular.module('awsTestApp')
         });
 
         awsFactory.getInstances.get(function (data) {
-            modalInstance.dismiss('cancel');
+            loadingModal.dismiss('cancel');
             angular.forEach(data.Reservations, function (res) {
                 res.Instances[0].InstanceName = '';
                 var arr = res.Instances[0].Tags;
-                for (var i = 0; i < arr.length; i++) {
-                    if (arr[i].Key == "Name") {
-                        res.Instances[0].InstanceName = arr[i].Value;
-                        console.log(res.Instances[0].InstanceName + "-----------" + res.Instances[0].KeyName);
-                    }
-
-                }
+//                Check Tag object 'Name' and set it to InstanceName
+                for (var i = 0; i < arr.length; i++) if (arr[i].Key == "Name") res.Instances[0].InstanceName = arr[i].Value;
 
                 $scope.instances.push(res);
             });
 
         });
 
-        $scope.terminate = function () {
-            alert('Terminated');
+        $scope.terminate = function (i) {
+            alert('Terminate ' + i.Instances[0].KeyName);
         }
 
-        $scope.stop = function () {
-            alert('Stop');
+        $scope.stop = function (i) {
+            alert('Stop ' + i.Instances[0].KeyName);
         }
 
         $scope.details = function () {
